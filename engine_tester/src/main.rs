e6e257468c76a000607015b2f1fd0eadfecaaa12
AstroidusTv <asteroidus@protonmail.com>
2023-12-13 14:30:33 +0100

    add primary files

---
+++ engine_tester/src/main.rs
@@ -0,0 +1,15 @@
+use window::graphics::window::Window;
+
+fn main() {
+    let mut window = Window::new(1080, 720, "Hello Window!");
+    window.init_gl();
+
+    while !window.should_close() {
+        unsafe {
+            gl::ClearColor(0.3, 0.5, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.update();
+    }
+}
+
